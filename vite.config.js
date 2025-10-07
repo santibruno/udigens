90ccc9656fcaaca3d1b54605f3179199c8973435
santibruno <santib1502@gmail.com>
2025-10-07 00:11:05 -0300

    se solucionan rutas

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   server: { port: 5173 },
   build: {
     outDir: 'dist',
     sourcemap: false
-  }
+  }  ,
+  plugins: [react()],
+  base: './', 
 })
